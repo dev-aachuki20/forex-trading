7fe0a039c968b600e960441e09bd7a4b4cff1061
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2023-07-24 18:36:49 +0530

    setup project

---
+++ app/Models/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens, Notifiable, HasRoles;
+
+    public $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'name',
+        'email',
+        'phone',
+        'dob',
+        'gender',
+        'password',
+        'remember_token',
+        'email_verified_at',
+
+        
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+        'email_verified_at',
+    ];
+
+    public function uploads()
+    {
+        return $this->morphMany(Uploads::class, 'uploadsable');
+    }
+
+  
+    public function getIsSuperAdminAttribute()
+    {
+        return $this->hasRole('super admin');
+    }
+
+    public function getIsUserAttribute()
+    {
+        return $this->hasRole('user');
+    }
+
+    public function profileImage()
+    {
+        return $this->morphOne(Uploads::class, 'uploadsable')->where('type', 'profile');
+    }
+
+    public function getProfileImageUrlAttribute()
+    {
+        if ($this->profileImage) {
+            return $this->profileImage->file_url;
+        }
+        return "";
+    }
+}
