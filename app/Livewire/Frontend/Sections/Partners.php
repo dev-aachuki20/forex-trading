83d1ab79d49f6c3287f63e466a9a6b25ebc95c65
Aachukihipl <aachukiagarwal.hipl@gmail.com>
2023-09-05 09:18:56 +0530

    changes in query

--- app/Livewire/Frontend/Sections/Partners.php
+++ app/Livewire/Frontend/Sections/Partners.php
@@ -13,7 +13,12 @@ class Partners extends Component
 
     public function render()
     {
-        $this->partnerslogo = PartnerLogo::where('language_id', $this->localeid)->where('status', 1)->orWhere('language_id', null)->get();
+        // $this->partnerslogo = PartnerLogo::where('language_id', $this->localeid)->where('status', 1)->orWhere('language_id', null)->get();
+        $this->partnerslogo = PartnerLogo::where('status', 1)
+            ->where(function ($query) {
+                $query->where('language_id', $this->localeid)
+                    ->orWhereNull('language_id');
+            })->get();
         return view('livewire.frontend.sections.partners');
     }
 }
