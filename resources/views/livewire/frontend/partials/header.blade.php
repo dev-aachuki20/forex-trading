7bb740e8e7c73b3d10b3d605b892dd5b87187722
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-02-16 17:21:16 +0530

    changes in header

--- resources/views/livewire/frontend/partials/header.blade.php
+++ resources/views/livewire/frontend/partials/header.blade.php
@@ -4,7 +4,7 @@
     <div class="v2-cookie">
         <div class="v2-cookie-disclaimer">
             <div class="rte--output">{{__('By using our website you agree to our use of cookies in accordance with our') }}
-                <a href="{{ route('other-page','cookies-policy') }}">{{__('cookie policy')}}</a>.
+                <a href="{{ route('other-page',['lang'=> app()->getLocale() ,'pageName' => 'cookies-policy']) }}">{{__('cookie policy')}}</a>.
             </div>
             <a class="v2-btn custom-btn fill-btn" data-ref="cookie-disclaimer__close" wire:click="setCookies">{{__('Okay')}} </a>
         </div>
